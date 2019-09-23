50a06e88e999ce1b956f2259693ad3b5a1b7d329
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2019-09-23 13:07:30 +0530

    Added more calculator functionality

--- Himanshu/HTML/javascript.js
+++ Himanshu/HTML/javascript.js
@@ -1,14 +1,36 @@
-var num;
-showInput = (obj)=>{
-    num = obj;
-    document.getElementById('result').innerHTML += obj;
+class Calculator{
+    constructor(){
+        this.num = ""
+    }
+    getInput = (obj)=>{
+        this.num += obj
+        let len = this.num.length-1;
+        if(obj == 0 && this.num == ""){
+            return;
+        }
+        document.getElementById('result').innerHTML += obj      
+    }
+
+    getOperation = (obj) =>{
+        this.num += obj
+        document.getElementById('result').innerHTML = ""
+    }
+
+    showResult = ()=>{
+        let result = eval(this.num)
+        document.getElementById('result').innerHTML = result
+        this.num = 0
+    }
+
+    clearOperation = ()=>{
+        this.num = 0
+        document.getElementById('result').innerHTML = ""
+    }
+
+    clearLastElement = ()=>{
+        this.num = this.num.slice(0,this.num.length-1);
+        document.getElementById('result').innerHTML = this.num;
+    }
 }
 
-giveResult = ()=>{
-    let data,result;
-    data = document.getElementById('result').value;
-    // console.log(data)
-    // console.log(typeof(data))
-    // result = eval(data);
-    // document.getElementById('result').innerHTML = result;
-}
+const cal = new Calculator()

b231cfc9f59a5d3b29ff4fd068218d718cd02ef6
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2019-10-01 19:56:28 +0530

    edited config.js

--- Himanshu/Web API Node JS/config/config.js
+++ Himanshu/Web API Node JS/config/config.js
@@ -1,9 +1,14 @@
+const { objectParser} = require('./utils/utils')
+
+const obj = objectParser(process.argv)
+console.log(obj)
+
 module.exports = {
-    PORT: process.env.PORT || configObj.PORT || 9000,
-    HOST: process.env.HOST || configObj.HOST || "0.0.0.0",
-    baseURI: process.env.baseURI || configObj.baseURI || "/api",
+    PORT: process.env.PORT || obj.PORT || 9000,
+    HOST: process.env.HOST || obj.HOST || "0.0.0.0",
+    baseURI: process.env.baseURI || obj.baseURI || "/api",
     database: {
-        DB_USER: process.env.DB_USER || configObj.DB_USER || "admin",
-        DB_PASSWORD: process.env.DB_PASSWORD || configObj.DB_PASSWORD || "root"
+        DB_USER: process.env.DB_USER || obj.DB_USER || "admin",
+        DB_PASSWORD: process.env.DB_PASSWORD || obj.DB_PASSWORD || "root"
     }
 }
